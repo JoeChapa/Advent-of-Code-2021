b5b95a21723a01612dd071a972f49db7ed8d16cd
Joe Chapa <jos.chapa@yahoo.com>
2021-12-31 19:04:00 -0600

    Started Day 12
    Still working on a solution to Day 12. This is by far the hardest challenge yet. So far I can create a map of the input data using a dictionary. I can also create a nest list of paths. I think next is to create a function to unnest the list to clearly display the paths (mostlyy for troubleshooting). I also have to figure out how to return to a big letter when generating the paths.

---
+++ Day 12/Day 12.py
@@ -0,0 +1,55 @@
+#First, let's get our input data
+file = open("Day 12 Input.txt", 'r')
+input_data = file.readlines()
+file.close()
+
+
+
+def find_connection(input_line):
+    directions = input_line.strip().split("-")
+    return directions
+
+def generate_map(connections):
+    paths={}
+    for connection in connections:
+        start, end = find_connection(connection)
+
+        #We have to check if the value is in paths already. If yes, we append a path, if not, we must add the value
+        if start not in paths.keys():
+            paths[start] = [end]
+        else:
+            paths[start] = paths[start] + [end]
+    return paths
+
+cave_map = generate_map(input_data)
+#print(cave_map)
+
+#Next we have to walk through the map. I think we should create a generate_paths() and then a valid_path(paths)? and lastly count the paths len of valid_paths
+
+def generate_paths(map, start_point):
+    path = [start_point]
+    for i in map[start_point]:
+        try:
+            path.append(generate_paths(map, i))
+        except:
+            if i == 'end':
+                path.append(i)
+    return path
+
+list_of_paths = generate_paths(cave_map, 'start')
+print(list_of_paths)
+
+#Takes a list of paths and only returns the valid ones
+#We are going to filter the list multiple times
+#First, to only find paths that start and end with start and end
+#Next, remove duplicates
+#secondly to make sure no small cave is repeated twice
+'''
+valid_path(paths_list):
+    valid_paths = []
+    for path in paths_list:
+        if path[1] == 'start and path[-1] == 'end'
+        valid_paths.append(path)
+        
+
+'''
